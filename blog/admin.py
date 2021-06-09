d68968a28f8ecc444b786f3a3db088d533f0cb74
IanMaguithi <maguithian13427@gmail.com>
2021-06-09 11:49:42 +0300

    initial blog commit

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 from blog.models import Article
+
+
 # Register your models here.
 class ArticleAdmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(Article, ArticleAdmin)
