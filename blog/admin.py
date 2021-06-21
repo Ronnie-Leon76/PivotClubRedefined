3f223bbb6f1c86f6107f5ffe447743cd521aa04d
IanMaguithi <maguithian13427@gmail.com>
2021-06-21 17:50:52 +0300

    blog new file

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from rangefilter.filters import DateRangeFilter, DateTimeRangeFilter
+from rangefilter.filters import DateTimeRangeFilter
 
-from blog.models import Article
+from blog.models import Article, Genre, ArticleReview
 
 
 # Register your models here.
@@ -9,13 +9,15 @@ class ArticleAdmin(admin.ModelAdmin):
     actions = ['approve_comments']
     list_display = ["title", 'author', 'is_reviewed', 'last_modified', 'created_on']
     prepopulated_fields = {'slug': ('title',), }
-    readonly_fields = ('photo', 'title', 'body', 'author', 'claps')
+    # readonly_fields = ('photo', 'title', 'body', 'author', 'claps')
     list_filter = (
         ('created_on', DateTimeRangeFilter), ('last_modified', DateTimeRangeFilter),
     )
 
-    def approve_comments(self, request, queryset):
+    def approve_comments(self, queryset):
         queryset.update(status=True)
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Genre)
+admin.site.register(ArticleReview)
